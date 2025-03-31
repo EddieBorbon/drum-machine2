36f603a1501d7dcd015db1ae7e5480091dcd3117
Eddie Jonathan García Borbón <eddiefloat@gmail.com>
2025-04-01 02:07:58 +0800

    upgrade

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/drum-machine2/', // Añade el nombre de tu repositorio aquí
-});
